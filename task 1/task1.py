041fa6b2b02e7f951eb54b979ab30bd82bd7d8a1
Suraj Kumar <suraj110905@gmail.com>
2026-02-14 02:30:39 +0530

    Added new files

--- task 1/task1.py
+++ task 1/task1.py
@@ -1,3 +1,4 @@
 # load an image
 # convert image into grayscale
-#
+# show and save image 
+# challenges jo bhi image show hoga uska user se input me uski location leni hai dusri chij user se puchna hai ki image ko save karna hai ya show karn hai 
