22292c4335133e334b65f1004138f182831a5d53
Suraj Kumar <suraj110905@gmail.com>
2026-02-14 15:17:08 +0530

    task 1

--- task 1/task1.py
+++ task 1/task1.py
@@ -1,4 +1,19 @@
 # load an image
 # convert image into grayscale
 # show and save image 
-# challenges jo bhi image show hoga uska user se input me uski location leni hai dusri chij user se puchna hai ki image ko save karna hai ya show karn hai 
+# challenges jo bhi image show hoga 1) take location input from user
+                            #       2) ask user that he wants to show image or save the image.
+                        #           3) if user wants to save the image then take the output name also from user
+                           #           then show a message file name saved successfully
+                           
+                           
+import cv2
+image_path = input("Enter the image path")#taking location input from the user
+image = cv2.imread(image_path)
+if image is not None:
+    cv2.imshow("Image preview",image)
+    cv2.waitKey(0)
+    cv2.namedWindow()
+else:
+    print("image not loaded")
+    
