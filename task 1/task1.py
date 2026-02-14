d3a5efd9fed419673208790404971c82c3137f98
Suraj Kumar <suraj110905@gmail.com>
2026-02-14 15:47:39 +0530

    task 1 completed

--- task 1/task1.py
+++ task 1/task1.py
@@ -8,12 +8,30 @@
                            
                            
 import cv2
-image_path = input("Enter the image path")#taking location input from the user
+
+#taking location of image input from user basically image path
+
+image_path = input("Enter the image path")
 image = cv2.imread(image_path)
-if image is not None:
-    cv2.imshow("Image preview",image)
-    cv2.waitKey(0)
-    cv2.namedWindow()
+choice = input("If you want to show image press 1 or for saving press 2")
+
+#asking from user choice that if he wants to preview image or save image
+if choice == '1':
+   # for choice 1 previewing the image 
+    if image is not None:
+        cv2.imshow("Image preview",image)
+        cv2.waitKey(0)
+        cv2.namedWindow()
+    else:
+        print("image not loaded")
 else:
-    print("image not loaded")
-    
+    # for choice 2 saving the image
+    if image is not None:
+        output = input("Enter the output file name") #taking the saving file name input from user
+        saved_image = cv2.imwrite(output,image)
+        #display message and file name by which image is saved
+        if saved_image:
+            print("saved successfully as :",output)
+        else:
+            print("image not saved")
+        
