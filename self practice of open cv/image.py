781e757eb5d385987a5d2ba55cca3dbdfd2b6a70
Suraj Kumar <suraj110905@gmail.com>
2026-02-23 00:00:08 +0530

    updates task 1 completed

--- self practice of open cv/image.py
+++ self practice of open cv/image.py
@@ -1,5 +1,5 @@
 import cv2
-image = cv2.imread("images\img5.jpg")
+image = cv2.imread("self practice of open cv\images\Screenshot 2025-11-17 015820.png")
 print("shape",image.shape)
 print("size",image.size)
 print("data type",image.dtype)
