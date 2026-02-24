66dde529a44a3f2b9c059b43e569473dc640c5e0
Suraj Kumar <suraj110905@gmail.com>
2026-02-24 23:44:37 +0530

    update

--- self practice of open cv/draw_rectangle.py
+++ self practice of open cv/draw_rectangle.py
@@ -1,7 +1,7 @@
 import cv2
 
 image = cv2.imread("self practice of open cv\images\Screenshot 2025-11-17 015820.png")
-print("pixels",image.)
+print("pixels",image)
 cv2.rectangle(image,(0,0),(100,100),(0,255,0),3)
 cv2.imshow("rectangle image",image)
 cv2.waitKey(0)
