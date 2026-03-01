f9a11f759cf3c5bb2b7a38e67f53bf27816b02fb
Suraj Kumar <suraj110905@gmail.com>
2026-03-02 03:06:27 +0530

    updates in file and code

--- self practice of open cv/writing_text.py
+++ self practice of open cv/writing_text.py
@@ -1,2 +1,6 @@
 import cv2
 image = cv2.imread("self practice of open cv\images\Screenshot 2025-11-17 015820.png")
+cv2.putText(image,"hello man",(10,40),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+cv2.imshow("text image",image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
