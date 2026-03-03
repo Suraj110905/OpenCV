ee64c8a9e142306de081c9de617d597aca01a3d6
Suraj Kumar <suraj110905@gmail.com>
2026-03-03 23:34:58 +0530

    writing text update

--- self practice of open cv/writing_text.py
+++ self practice of open cv/writing_text.py
@@ -1,3 +1,4 @@
+#writing text in image
 import cv2
 image = cv2.imread("self practice of open cv\images\Screenshot 2025-11-17 015820.png")
 cv2.putText(image,"hello man",(10,40),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
